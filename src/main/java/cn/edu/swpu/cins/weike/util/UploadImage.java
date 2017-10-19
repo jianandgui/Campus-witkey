f47fd1b35a7eae1b5895b3acd00d8785e606fb9c
muyi <879604213@qq.com>
2017-10-19 21:30:48 +0800

    修改文件返回类型

--- src/main/java/cn/edu/swpu/cins/weike/util/UploadImage.java
+++ src/main/java/cn/edu/swpu/cins/weike/util/UploadImage.java
@@ -2,6 +2,7 @@ package cn.edu.swpu.cins.weike.util;
 
 import cn.edu.swpu.cins.weike.enums.ExceptionEnum;
 import cn.edu.swpu.cins.weike.exception.ProjectException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -10,18 +11,25 @@ import java.io.IOException;
 
 @Service
 public class UploadImage {
+
+    @Value("${weike.images.location}")
+    private String path;
+
     public String uploadImage(MultipartFile image, String username) throws IOException {
-//        String path = "/home/yang/file";
-        String path = "/home/tangxudong/images";
         String fileName =null;
         fileName = image.getOriginalFilename().substring(image.getOriginalFilename().lastIndexOf("."));
         path += "/" +username.hashCode()+fileName;
         File file = new File(path);
         try {
             image.transferTo(file);
+            path = getPath(path);
             return path;
         } catch (IOException e) {
             throw new ProjectException(ExceptionEnum.FILE_UPLOAD_FAILED.getMsg());
         }
+    }
+
+    public String getPath(String path) {
+        return path.replaceFirst("/home/tangxudong", "");
     }
 }
